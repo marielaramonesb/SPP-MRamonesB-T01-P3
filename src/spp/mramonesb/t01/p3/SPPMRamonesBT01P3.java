dfb23ec0576b3e8b7113873fdbe62d422592317e
marielaramonesbalvoa <marielaramonesbalvoa@10.34.157.204>
2017-01-31 00:39:52 -0600

    distancia entre 2 puntos

---
+++ src/spp/mramonesb/t01/p3/SPPMRamonesBT01P3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp.mramonesb.t01.p3;
+import java.util.Scanner;
+/**
+ *
+ * @author marielaramonesbalvoa
+ */
+public class SPPMRamonesBT01P3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declaro variables 
+    double x1, y1, x2, y2, op;
+    // pido los datos 
+    Scanner kb = new Scanner (System.in);
+        System.out.println("Insertar valor de x1");
+        x1= kb.nextDouble();
+        System.out.println("Insertar valor de x2");
+        y1= kb.nextDouble();    
+        System.out.println("Insertar valor de y1");
+        x2= kb.nextDouble();    
+        System.out.println("Insertar valor de y2");
+        y2= kb.nextDouble();    
+    
+    //operaciones 
+    op = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
+    System.out.println("La distancia es igual a " + op);
+    }
+ 
+    
+    
+}
